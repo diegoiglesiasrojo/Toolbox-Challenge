b804cc83d4a6f9b4eb6051935dcba0619ff9ccb5
Diego Iglesias <diegoiglesiasrojo@gmail.com>
2024-02-18 05:00:07 -0300

    Small fixes on dataControllers file.

--- controllers/dataControllers.js
+++ controllers/dataControllers.js
@@ -19,26 +19,37 @@ const dataControllers = {
       // Format data
       const arrayOfData = initialArrayOfData.map((arr) => {
         // If some object don't have all keys, delete this object
+        const arrWithoutBadObjects = arr.filter((objData) => {
+          if (objData.file && objData.text && objData.number && objData.hex) {
+            if (
+              objData.file != "" &&
+              objData.text != "" &&
+              objData.number != "" &&
+              objData.hex != ""
+            ) {
+              return true;
+            } else {
+              return false;
+            }
+          } else {
+            false;
+          }
+        });
+
+        // Create new object structure
         const formatedObj = {
           file: arr[0]?.file,
-          lines: arr.filter((objData) => {
-            if (objData.file && objData.text && objData.number && objData.hex) {
-              if (
-                objData.file != "" &&
-                objData.text != "" &&
-                objData.number != "" &&
-                objData.hex != ""
-              ) {
-                return true;
-              } else {
-                return false;
-              }
-            } else {
-              false;
-            }
+          lines: arrWithoutBadObjects.map((obj) => {
+            const objWithoutFileProperthy = {
+              text: obj.text,
+              number: obj.number,
+              hex: obj.hex,
+            };
+            return objWithoutFileProperthy;
           }),
         };
 
+        // Check that the object is correct
         if (formatedObj.lines.length > 0 && arr.length > 0 && arr !== null) {
           return formatedObj;
         }
